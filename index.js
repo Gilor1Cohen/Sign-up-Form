aa1b3793380820a8cccd0d4ea9295f80cc5982d9
Gilor Cohen <gilor6811@gmail.com>
2024-05-16 10:49:07 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let container = document.querySelector(".container");
+let bg = document.querySelector(".sign-up");
+
+bg.addEventListener("click", function bg() {
+  container.classList.toggle("change");
+});
+
